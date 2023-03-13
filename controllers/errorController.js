71fa1aee8e8b415a0743c157414b38878501cfec
abdiaziiz-Arrale <abdiaziizarrale@gmail.com>
2023-03-13 17:29:11 +0300

    Fixing problem in Error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,35 +6,15 @@ const handleCastErrorDB = err => {
 };
 
 module.exports = (err, req, res, next)=>{
-    console.log(err.stack);
+
     err.statusCode = err.statusCode ||500;
 
     err.status= err.status || 'error';
 
-   if ( process.env.NODE_ENV === 'production'){
-    // in operational send messages to clients
-    let error ={...err}
-    if (error.name==='CastError') error =handleCastErrorDB(error);
-    if(err.isOperation){
-           res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-
-
-      
-      }) 
-    }
-    //programiing or other Unknown error
-    else{
-        console.error('ERROR 🪓',err)
-        res.status(err.statusCode).json({
-            status:"error",
-            message:"Something went very wrong"
-        })
-    }
-
-    }
-    else if ( process.env.NODE_ENV === 'devolopment'){
+ 
+     if ( process.env.NODE_ENV === 'devolopment'){
+      let error ={...err}
+      if (error.name==='CastError') error =handleCastErrorDB(error);
             res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
@@ -43,6 +23,29 @@ module.exports = (err, req, res, next)=>{
     
     })
     }
+    else if ( process.env.NODE_ENV === 'production'){
+      // in operational send messages to clients
+      let error ={...err}
+      if (error.name==='CastError') error =handleCastErrorDB(error);
+      if(err.isOperation){
+             res.status(err.statusCode).json({
+          status: err.status,
+          message: err.message,
+  
+  
+        
+        }) 
+      }
+      //programiing or other Unknown error
+      else{
+          console.error('ERROR 🪓',err)
+          res.status(err.statusCode).json({
+              status:"error",
+              message:"Something went very wrong"
+          })
+      }
+  
+      }
 
   
   }
